da23f68d0151ea02f9152635cf21fa14bd52a3f0
rce3986 <rce3986@gmail.com>
2022-03-06 05:40:50 -0500

    Initial Commit

---
+++ Bad Habit Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bad Habit Counter
+//
+//  Created by Ryan Elliott on 3/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
